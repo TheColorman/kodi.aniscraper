94247487de60d786cc862cfc100b4bb41ab3be29
TheColorman <thoren.alex@gmail.com>
2023-08-26 23:47:06 +0200

    🚧 Create initial AniList request system

--- metadata.aniscraper/main.py
+++ metadata.aniscraper/main.py
@@ -13,20 +13,27 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# TODO: Rate limits
+# TODO: File not found errors
+# TODO: Handle 404s
+
 import sys, os
 import urllib.parse
 import xml.etree.ElementTree as ET
+import requests
 
 import xbmcgui
 import xbmcplugin, xbmcaddon
 import xbmc, xbmcvfs
+
+import anitopy
+import web_pdb
 
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
-import web_pdb
 
 def get_params():
     param_string = sys.argv[2][1:]
@@ -51,21 +58,27 @@ class Main:
     def __init__(self):
         # Properties
         try:
-            self._db = pickle.load(open(__picklejar__, 'rb'))
+            self.initjar()
         except Exception as e:
-            log("Exception thrown loading the jar, creating empty jar: " + str(e))
-            self._db = {}
-            self.updatejar()
+            log("Exception thrown initializing the picklejar: " + str(e))
     
-    def loadjar(self):
+    def initjar(self):
         """Load the database from the picklejar"""
         try:
+            if not xbmcvfs.exists(__profile__):
+                log("Profile folder does not exist, creating it")
+                xbmcvfs.mkdir(__profile__)
+
+            if not xbmcvfs.exists(__picklejar__):
+                log("Picklejar does not exist, creating empty jar")
+                pickle.dump({}, open(__picklejar__, 'wb'), pickle.HIGHEST_PROTOCOL)
+
             fs = open(__picklejar__, 'rb')
             self._db = pickle.load(fs)
             fs.close()
             return True
         except Exception as e:
-            log("Exception thrown loading the jar: " + str(e))
+            log("Exception thrown initializing the picklejar: " + str(e))
             return False
 
     def updatejar(self):
@@ -103,43 +116,130 @@ class Main:
 
             raise Exception(f"No folder with name \"{folder_name}\" found in any of the video sources in sources.xml")
     
-    def scan_anime(self, folder_path):
+    def scan_anime(self, folder_path: str) -> str:
         """Scan a folder for anime. Returns a dictionary with keyr = anime titles and values = list of absolute paths to episode files"""
-        anime = {}
-        def resolve_anime(filename):
-            pass
+        def parse_anime(filename: str) -> str:
+            parsed = anitopy.parse(filename)
+            log("Parsed " + filename + " to " + str(parsed))
+            return parsed['anime_title']
 
-        def scan(folder):
+        anidict = {}
+        def scan(folder: str) -> dict:
             for item in os.listdir(folder):
+                log("Scanning " + item)
                 item_path = os.path.join(folder, item)
                 if os.path.isdir(item_path):
+                    log("Recursing into " + item)
                     scan(item_path)
                 elif os.path.isfile(item_path):
+                    log("Found file " + item)
                     if item.endswith('.mkv') or item.endswith('.mp4'):
-                        anime_title = resolve_anime(item)
-                        if anime[anime_title] is None:
-                            anime[anime_title] = []
-                        anime[anime_title].append(item_path)
+                        anime_title = parse_anime(item)
+                        anidict.setdefault(anime_title, [])
+                        anidict[anime_title].append(item_path)
+            
+        scan(folder_path)
+        return anidict
+    
+    def sort_most_common_key(self, d: dict) -> str:
+        """Return the dict sorted by key with the longest list"""
+        return sorted(d.items(), key=lambda x: len(x[1]), reverse=True)
 
+    def AL_get_anime_by_title(self, title: str):
+        """Uses the AniList API to search for anime by title"""
+        log("Using AniList API to search for anime: " + title)
+        query = '''
+        query ($search: String) {
+            Media (search: $search, type: ANIME) {
+                id
+                idMal
+                title {
+                    english
+                    romaji
+                }
+                format
+                description
+                season
+                coverImage {
+                    extraLarge
+                    medium
+                }
+                bannerImage
+                averageScore
+                popularity
+                siteUrl
+            }
+        }
+        '''
+        variables = {
+            'search': title
+        }
+        url = 'https://graphql.anilist.co'
+
+        response = requests.post(url, json={'query': query, 'variables': variables})
+
+        json = response.json()
+        if json.get('errors'):
+            raise Exception(json['errors'][0]['message'])
+        else:
+            anime = json['data']['Media']
+            log(f"Anime with id {anime['id']} found!")
+            return anime
+    
+    def fetch_anime_by_title(self, title: str, no_cache=False):
+        """Fetch anime by title from the database, or from the AniList API if not found"""
+        self._db.setdefault('anime', {})
+        if self._db['anime'].get(title) and not no_cache:
+            log("Found anime in database")
+            return self._db['anime'][title]
+        else:
+            log(f"Fetching {title} from AniList API")
+            try:
+                anime = self.AL_get_anime_by_title(title)
+                self._db['anime'][title] = anime
+                self.updatejar()
+                return anime
+            except Exception as e:
+                log("Failed to fetch anime from AniList API: " + str(e))
+                return None
 
 main = Main()
 
 if action == 'find':
-    log('Find callback')
     title = params['title']
+    log(f'Find anime with title "{title}"')
     anime_folder = os.path.join(main.sourcepath(title), title)
-    web_pdb.set_trace()
-    year = params.get('year', 'not specified')
-    xbmc.log(f'Find movie with title "{title}" from year {year}', xbmc.LOGDEBUG)
+    anime_candidates = main.scan_anime(anime_folder)
 
-    liz = xbmcgui.ListItem('Demo show 1', offscreen=True)
-    liz.setArt({'thumb': 'DefaultVideo.png'})
-    liz.setProperty('relevance', '0.5')
-    xbmcplugin.addDirectoryItem(handle=plugin_handle, url='/path/to/show', listitem=liz, isFolder=True)
-    liz = xbmcgui.ListItem('Demo show 2', offscreen=True)
-    liz.setArt({'thumb': 'DefaultVideo.png'})
-    liz.setProperty('relevance', '0.3')
-    xbmcplugin.addDirectoryItem(handle=plugin_handle, url='/path/to/show2', listitem=liz, isFolder=True)
+    anime_candidates = main.sort_most_common_key(anime_candidates)
+    for title, episodes in anime_candidates:
+        anime = main.fetch_anime_by_title(title, no_cache=True) #! Remove no_cache=True in production
+        log(f"Got {str(anime)}")
+        if anime is not None:
+            break
+    
+    if anime is None:
+        raise Exception("No anime found for title " + title)
+    # year = params.get('year', 'not specified')
+
+    liz = xbmcgui.ListItem(anime['title']['english'], anime['title']['romaji'], offscreen=True)
+    liz.setArt({
+        'thumb': anime['coverImage']['medium'],
+        'poster': anime['coverImage']['extraLarge'],
+        'banner': anime['bannerImage'],
+        'landscape': anime['bannerImage']
+    })
+    liz.setUniqueIDs({
+        'anilist': anime['id'],
+        'myanimelist': anime['idMal']
+    }, "anilist")
+    liz.setRating("anilist average", anime['averageScore'], defaultt=True)
+    xbmcplugin.addDirectoryItem(
+        handle=plugin_handle,
+        url=str(anime['id']),
+        listitem=liz,
+        isFolder=True
+    )
 
 elif action == 'getdetails':
     url = params['url']
